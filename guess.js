dcd1a75b794e33656773c72219b1425cb81abb02
RiouC <riou.christophe@protonmail.com>
2021-03-12 17:16:33 +0100

    guess: don't return values, print with console.log

--- guess.js
+++ guess.js
@@ -1,15 +1,15 @@
 
 const guess = (userGuess, secret) => {
     if (userGuess < secret)
-	return "Too small!";
+	console.log("Too small!");
     else if (userGuess > secret)
-	return "Too big!";
+	console.log("Too big!");
     else
-	return "You win";
+	console.log("You win");
 }
 
-// console.log(guess(50, 14));   --> Too big!
-// console.log(guess(27, 78));   --> Too small!
-// console.log(guess(32, 32));   --> You win
-// console.log(guess(89, 98));   --> Too small!
+// guess(50, 14)//;   --> Too big!
+// guess(27, 78)//;   --> Too small!
+// guess(32, 32)//;   --> You win
+// guess(89, 98)//;   --> Too small!
 
